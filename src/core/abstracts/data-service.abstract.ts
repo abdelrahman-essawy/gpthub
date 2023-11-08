1bf549d28e7e7f02c251ea62ff6b7bf444f53ecc
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-11-08 09:12:58 +0200

    feat; update abstracts

--- src/core/abstracts/data-service.abstract.ts
+++ src/core/abstracts/data-service.abstract.ts
@@ -1,3 +1,4 @@
+import { Message } from '../entities/message.entity';
 import { Room } from '../entities/room.entity';
 import { User } from '../entities/user.entity';
 import { IRepository } from './repository.abstract';
@@ -5,4 +6,6 @@ import { IRepository } from './repository.abstract';
 export abstract class IDataService {
   abstract users: IRepository<User>;
   abstract rooms: IRepository<Room>;
+  abstract resources: IRepository<Response>;
+  abstract messages: IRepository<Message>;
 }
