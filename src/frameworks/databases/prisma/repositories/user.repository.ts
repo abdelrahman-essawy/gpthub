27b98eb4ae3b18395d4d39abaa6c2ba8ef1c16c0
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-11-13 11:29:21 +0200

    stash

--- src/frameworks/databases/prisma/repositories/user.repository.ts
+++ src/frameworks/databases/prisma/repositories/user.repository.ts
@@ -1,15 +1,93 @@
-// src/repositories/user.repository.ts
+// src/repositories/prisma-user.repository.ts
 
-import { PrismaClient } from '@prisma/client';
+// @ts-nocheck
+import { PrismaClient, User } from '@prisma/client';
 import { IRepository } from 'src/core/abstracts/repositories/repository.abstract';
-import { User } from 'src/core/entities/user.entity';
+import { IUserRepository } from 'src/core/abstracts/repositories/user-repository.abstract';
 import { PrismaRepository } from '../repository';
 
 export class PrismaUserRepository
   extends PrismaRepository
-  implements IRepository<User>
-{
+  implements IUserRepository {
+  private prisma: PrismaClient;
+
   constructor(prisma: PrismaClient) {
     super(prisma);
+  }
+
+  async find(options: any): Promise<User[]> {
+    // Implement your logic using Prisma queries
+    return this.prisma.user.findMany({
+      // add your Prisma query options here
+    });
+  }
+
+  async findByUsername(username: string, options: any): Promise<User | null> {
+    // Implement your logic using Prisma queries
+    return this.prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+  }
+
+  async findByEmail(email: string, options: any): Promise<User | null> {
+    // Implement your logic using Prisma queries
+    return this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+  }
+
+  async isEmailExists(email: string): Promise<boolean> {
+    const user = await this.findByEmail(email);
+    return user !== null;
+  }
+
+  async isUsernameExists(username: string): Promise<boolean> {
+    const user = await this.findByUsername(username);
+    return user !== null;
+  }
+
+  async findByUsernameOrEmail(
+    usernameOrEmail: string,
+    options: any,
+  ): Promise<User | null> {
+    // Implement your logic using Prisma queries
+    return this.prisma.user.findFirst({
+      where: {
+        OR: [
+          {
+            username: usernameOrEmail,
+          },
+          {
+            email: usernameOrEmail,
+          },
+        ],
+      },
+    });
+  }
+
+  async getUserPasswordByUsername(username: string): Promise<string | null> {
+    // Implement your logic using Prisma queries
+    const user = await this.findByUsername(username, {});
+    return user ? user.password : null;
+  }
+
+  async getUserPasswordByEmail(email: string): Promise<string | null> {
+    // Implement your logic using Prisma queries
+    const user = await this.findByEmail(email, {});
+    return user ? user.password : null;
+  }
+
+  async getUserPasswordById(id: string): Promise<string | null> {
+    // Implement your logic using Prisma queries
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: parseInt(id, 10),
+      },
+    });
+    return user ? user.password : null;
   }
 }
--- src/services/databases/databases-service.module.ts
+++ src/services/databases/databases-service.module.ts
@@ -3,7 +3,7 @@ import { MongoDatabaseServiceModule } from 'src/frameworks/databases/mongo/mongo
 import { PrismaDatabaseServiceModule } from 'src/frameworks/databases/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaDatabaseServiceModule],
-  exports: [PrismaDatabaseServiceModule],
+  imports: [MongoDatabaseServiceModule, PrismaDatabaseServiceModule],
+  exports: [MongoDatabaseServiceModule, PrismaDatabaseServiceModule],
 })
 export class DatabaseServicesModule { }
