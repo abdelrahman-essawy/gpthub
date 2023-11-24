1a34c5d552cc8e0d6198718e099db3963fc4d1a1
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-11-24 18:02:14 +0200

    logger init

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserUseCasesModule } from './use-cases/user/user-use-cases.module';
 import { UserController } from './controllers/user.controller';
 import { AuthController } from './controllers/auth.controller';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [UserUseCasesModule],
+  imports: [UserUseCasesModule, PrometheusModule.register()],
   controllers: [AuthController, UserController],
 })
 export class AppModule { }
