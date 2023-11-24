d232727cfc656d5d8303e1d69277a8eb1577f9da
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-11-24 14:34:35 +0200

    feat: resources controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserUseCasesModule } from './use-cases/user/user-use-cases.module';
 import { UserController } from './controllers/user.controller';
 import { AuthController } from './controllers/auth.controller';
+import { ResourcesModule } from 'apps/resources/src/resources.module';
 
 @Module({
-  imports: [UserUseCasesModule],
+  imports: [UserUseCasesModule, ResourcesModule],
   controllers: [AuthController, UserController],
 })
 export class AppModule { }
