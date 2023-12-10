5f8b2d46d47e504979e2b3fd16b394303a0c8532
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-12-11 01:19:30 +0200

    feat: dart protoc generator

--- scripts/protoc-generate.dart
+++ scripts/protoc-generate.dart
@@ -1,39 +1,57 @@
-import 'dart:io';
+#!/usr/bin/env bash
 
-void main() {
-  // Root directory of the Dart app
-  var rootDir = Process.runSync('git', ['rev-parse', '--show-toplevel']);
-  var ROOT_DIR = rootDir.stdout.trim();
+# Root directory of app
+ROOT_DIR=$(git rev-parse --show-toplevel)
 
-  // Path to protoc-gen-dart binary
-  var PROTOC_GEN_DART_PATH = "${ROOT_DIR}/path/to/protoc-gen-dart";
+# Directory holding all .proto files
+SRC_DIR="${ROOT_DIR}/libs/core/src/proto/src"
 
-  // Directory holding all .proto files
-  var SRC_DIR = "${ROOT_DIR}/src";
+# Directory to write generated code (.dart files)
+OUT_DIR="${ROOT_DIR}/libs/core/src/proto/generated_dart"
 
-  // Directory to write generated Dart code
-  var OUT_DIR = "${ROOT_DIR}/build/gen";
+# Clean all existing generated files
+rm -rf "${OUT_DIR}"
+mkdir -p "${OUT_DIR}"
 
-  // Clean all existing generated files
-  Process.runSync('rm', ['-rf', OUT_DIR]);
-  Process.runSync('mkdir', ['-p', OUT_DIR]);
+# Find all .proto files
+PROTO_FILES=($(find "${SRC_DIR}" -name "*.proto"))
 
-  // List of .proto files
-  var protoFiles = ["foo.proto", "bar/baz.proto"];
+# Count the total number of .proto files
+TOTAL_FILES=${#PROTO_FILES[@]}
 
-  // Generate Dart code for the specified .proto files
-  for (var protoFile in protoFiles) {
-    var protoPath = "$SRC_DIR/$protoFile";
-    var outputFilePath = "$OUT_DIR/${protoFile.replaceAll('.proto', '.pb.dart')}";
+# Counter for completed files
+COMPLETED_FILES=0
 
-    Process.runSync('protoc', [
-      '--dart_out=$OUT_DIR',
-      '--proto_path=$SRC_DIR',
-      protoPath,
-    ]);
+# Colors for formatting
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-    print("Generated Dart code for $protoPath to $outputFilePath");
-  }
+echo -e "${YELLOW}Generating Dart Files...${NC}"
 
-  print("Dart code generation completed in $OUT_DIR");
-}
+# Start time for measuring elapsed time
+START_TIME=$(date +%s)
+
+# Iterate over each .proto file
+for PROTO_FILE in "${PROTO_FILES[@]}"; do
+    FILENAME=$(basename -- "$PROTO_FILE")
+    FILENAME_NOEXT="${FILENAME%.*}"
+
+    echo -e "${GREEN}Processing $FILENAME...${NC}"
+
+    # Generate code for the current .proto file with various options
+    protoc \
+        --dart_out="${OUT_DIR}" \
+        --proto_path="${SRC_DIR}" \
+        "${PROTO_FILE}"
+
+    # Increment completed files counter
+    ((COMPLETED_FILES++))
+done
+
+echo -e "\n${YELLOW}Generation completed for $COMPLETED_FILES out of $TOTAL_FILES files.${NC}"
+
+# Display the time taken
+END_TIME=$(date +%s)
+ELAPSED_TIME=$((END_TIME - START_TIME))
+echo -e "${YELLOW}Time taken: $ELAPSED_TIME seconds.${NC}"
