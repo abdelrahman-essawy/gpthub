fe95006c3366b9e1fa625d6db2e229747d7eab1d
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-12-23 17:49:35 +0200

    feat: system interfaces Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

---
+++ libs/core/src/interfaces/resource.interface.ts
@@ -0,0 +1,40 @@
+export interface Resource {
+  id: string;
+  name: string;
+  description: string;
+  type: ResourceType;
+  format: ResourceFormat;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export enum ResourceType {
+  BOOK = 'BOOK',
+  VIDEO = 'VIDEO',
+  AUDIO = 'AUDIO',
+  IMAGE = 'IMAGE',
+  DOCUMENT = 'DOCUMENT',
+  ARTICLE = 'ARTICLE',
+  CODE = 'CODE',
+  OTHER = 'OTHER',
+}
+
+export enum ResourceFormat {
+  PDF = 'PDF',
+  DOC = 'DOC',
+  DOCX = 'DOCX',
+  XLS = 'XLS',
+  XLSX = 'XLSX',
+  PPT = 'PPT',
+  PPTX = 'PPTX',
+  MP3 = 'MP3',
+  MP4 = 'MP4',
+  WAV = 'WAV',
+  AVI = 'AVI',
+  MKV = 'MKV',
+  JPG = 'JPG',
+  JPEG = 'JPEG',
+  PNG = 'PNG',
+  GIF = 'GIF',
+  OTHER = 'OTHER',
+}
