90559ba4a967e144317662cd337c6137099e6537
faragzz <a.khaled46462@gmail.com>
2024-01-30 10:04:11 +0200

    test1

--- apps/backend/users/src/app/auth/decorators/roles.decorator.ts
+++ apps/backend/users/src/app/auth/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { UserRole } from '@core';
 import { SetMetadata } from '@nestjs/common';
 
-export const Roles = (roles: UserRole[]) => SetMetadata(UserRole, roes);
+export const Roles = (roles: UserRole[]) => SetMetadata(UserRole, roles);
 //ok
