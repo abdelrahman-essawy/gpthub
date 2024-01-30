46047c54df0d947d4b9a74c8de21b908e5354e55
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-01-31 01:28:27 +0200

    init
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

---
+++ apps/backend/auth/src/main.ts
@@ -0,0 +1,28 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'auth',
+        protoPath: 'apps/backend/auth/src/proto/auth.proto',
+      },
+    },
+  );
+
+  await app.listen();
+  Logger.log(`🚀 Application is running`);
+}
+
+bootstrap();
