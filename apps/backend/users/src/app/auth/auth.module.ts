278328224411605d692aa76306e6f737ca084dd4
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-01-30 04:27:05 +0200

    feat: husky
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/backend/users/src/app/auth/auth.module.ts
+++ apps/backend/users/src/app/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { HashingModule } from '@backend/hashing';
 import { StrategiesModule } from '@backend/strategies';
 
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule as NestJwtModule } from '@nestjs/jwt/dist/jwt.module';
+import { HashingModule } from '@backend/hashing';
 
 @Module({
   imports: [
