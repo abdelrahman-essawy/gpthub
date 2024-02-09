4808135d1b31830d0cd402fd918d973de43af9ff
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-02-10 01:49:33 +0200

    feat: config module
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

---
+++ libs/backend/config/src/lib/config.module.ts
@@ -0,0 +1,29 @@
+import { Global, Logger, Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as process from 'process';
+import { suitableEnvFilePath } from '@backend/utilities';
+
+import { ConfigService } from './config.service';
+
+@Global()
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+
+      envFilePath: suitableEnvFilePath,
+    }),
+  ],
+  providers: [ConfigService, Logger],
+  exports: [ConfigService],
+})
+export class ConfigModule implements OnModuleInit {
+  constructor(private readonly logger: Logger) {}
+
+  onModuleInit() {
+    this.logger.warn(
+      'ConfigModule has been initialized in NODE_ENV = ' +
+        process.env['NODE_ENV'],
+    );
+  }
+}
--- libs/backend/utilities/src/index.ts
+++ libs/backend/utilities/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/headers';
 export * from './lib/user-token-validation';
+export * from './lib/env';
