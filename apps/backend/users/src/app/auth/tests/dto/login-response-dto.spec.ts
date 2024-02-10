e95804af11761c15e9e5a5081bf8476af052e926
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-02-10 22:25:00 +0200

    feat: login-response.dto.ts test cases
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

---
+++ apps/backend/users/src/app/auth/tests/dto/login-response-dto.spec.ts
@@ -0,0 +1,29 @@
+import { LoginResponseDto } from '../../dto';
+import { userInDatabase } from '../mocks/login-responce-mocks';
+
+describe('LoginResponseDto', () => {
+  const accessToken = 'testAccessToken';
+  const refreshToken = 'testRefreshToken';
+
+  it('should create a new instance of LoginResponseDto with valid input', () => {
+    const loginResponseDto: LoginResponseDto = new LoginResponseDto(
+      userInDatabase,
+      accessToken,
+      refreshToken,
+    );
+    expect(loginResponseDto).toBeDefined();
+  });
+
+  it('should populate all fields correctly with valid input', () => {
+    const loginResponseDto: LoginResponseDto = new LoginResponseDto(
+      userInDatabase,
+      accessToken,
+      refreshToken,
+    );
+    expect(loginResponseDto.accessToken).toEqual(accessToken);
+    expect(loginResponseDto.refreshToken).toEqual(refreshToken);
+    expect(loginResponseDto.user).toEqual(
+      expect.objectContaining(userInDatabase),
+    );
+  });
+});
---
+++ apps/backend/users/src/app/auth/tests/mocks/login-responce-mocks.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+import { IUser, UserRole } from '@core';
+
+export const userInDatabase: IUser = {
+  id: faker.string.uuid(),
+  email: faker.internet.email(),
+  username: faker.internet.userName(),
+  password: faker.internet.password(),
+  verified: true,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  firstName: faker.person.firstName(),
+  role: UserRole.USER,
+  hashedRefreshToken: faker.internet.password(),
+  birthday: faker.date.past(),
+  lastName: faker.person.lastName(),
+};
